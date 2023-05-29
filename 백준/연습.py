2f0550b8e58009c0dc91e03324c6b573efc8ec55
ksw0733 <ksw0733@gmail.com>
2023-05-29 22:35:36 +0900

    `

--- 백준/연습.py
+++ 백준/연습.py
@@ -1,30 +1,32 @@
-## 백준 2606번
+## 백준 7569번
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
-n = int(input())
+m, n, h = map(int, input().split())
 
-x, y = map(int, input().split())
+dx = [0, 0, -1, 1, 0, 0]
+dy = [-1, 1, 0, 0, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
 
-m = int(input())
-graph = [[False] * (n + 1) for _ in range(n + 1)]
-visited = [False] * (n + 1)
-cnt = 0
+visited = [False]*((n*m*h)+1)
 
-def dfs(graph, x, y):
-    global cnt
-    cnt += 1
-    visited[x] = True
-    for i in range(m+1):
-        
-    
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+def dfs(x, y, z):
+    if  x<= -1 or x >= m or y <= -1 or y >= n or z <= -1 or z >= h:
+        return False
+    if graph[x][y][z] == 1:
+        graph[x][y][z] = 1
+        dfs(x-1, y, z)
+        dfs(x+1, y, z)
+        dfs(x, y-1, z)
+        dfs(x, y+1, z)
+        dfs(x, y, z-1)
+        dfs(x, y, z+1)
+        return True
+    return False
 
-dfs(graph, x, visited)
-
-print(cnt)
+graph = []
+for i in range(h):
+    for j in range(n):
+        graph.append(list(map(int, input().split())))

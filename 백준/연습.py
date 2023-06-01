dd05286a4fe27620a16a39070e4db7eb175281e3
ksw0733 <ksw0733@gmail.com>
2023-06-01 22:31:36 +0900

    `

--- 백준/연습.py
+++ 백준/연습.py
@@ -1,28 +1,48 @@
-## 백준 5014번
+## 백준 2468번
 import sys
 from collections import deque
 
 input = sys.stdin.readline
-
-f, s, g, u, d = map(int, input().split())
-
-building = [0] * (f + 1)
+sys.setrecursionlimit(10 ** 6)
 queue = deque()
 
-def bfs(s):
-    queue.append(s)
-    building[s] = 1
-    while queue:
-        x = queue.popleft()
-        
-        if x == g:
-            return building[x] -1
-        
-        for move in (x + u, x - d):
-            if 1 <= move <= f and building[move] == 0:
-                building[move] = building[x] + 1
-                queue.append(move)
-                
-    return 'use the stairs'
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0 for _ in range(m)] for _ in range(n)]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def dfs(x, y):
+    if x <= -1 or x > n or y <= -1 or y > m:
+        return False
+    if visited[x][y] == 0:
+        visited[x][y] = 1
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
 
-print(bfs(s))
+def ice_melting(x, y):
+    queue.append(x, y)
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == 0:
+                graph[x][y] -= 1
+                queue.append((nx, ny))
+            if graph[x][y] == 0:
+                queue.clear()
+                break
+            
+cnt = 0
+max_year = max(map(max, graph))
+
+for i in range(max_year):
+    for j in range(n):
+        for k in range(m):
+            if dfs(j, k) == True:
+                cnt += 1
+                
